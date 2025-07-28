30c1c032ae558b77dcf0efd3f502762c1ecb38b5
KevinButcher <akazuneko@gmail.com>
2025-07-28 13:11:57 -0600

    home page created

---
+++ Portfolio/Projects/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'Projects'
+urlpatterns = [
+    path('', views.index, name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Portfolio/Projects/views.py
+++ Portfolio/Projects/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
-from embed_video.fields import EmbedVideoField
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Project
+from django.template import loader
+# from .forms import PortfolioForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# from embed_video.fields import EmbedVideoField
 
 # Create your views here.
+def index(request):
+    context = {
+        "intro": ""
+    }
+    return render(request, 'Projects/index.html', context)
